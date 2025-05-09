36c7d10e60dcb35912081195427d6404cacc880d
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2025-05-08 21:45:01 -0400

    Arreglo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,29 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Base de datos conectada');
+    } catch (error) {
+        console.error('Error al conectar MongoDB', error);
+        process.exit(1);
+    }
+};
+
+export default conectarDB;
+
+// backend/src/index.js
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/auth.js';
 import servicioRoutes from './routes/servicios.js';
+
+dotenv.config();
 
 const app = express();
 
